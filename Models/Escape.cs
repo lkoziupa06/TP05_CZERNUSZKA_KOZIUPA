f7d53fde5374030274fec7bff7a5bb4007f03895
LUCAS NICOLAS KOZIUPA <47572827@ort>
2023-05-30 10:15:12 -0300

    incognitas

--- Models/Escape.cs
+++ Models/Escape.cs
@@ -9,10 +9,10 @@ public static class Escape{
     private static string[] incognitasSalas {get; set;} = new string [4];
     private static int estadoJuego {get;set;} = 1;
     private static void inicializarJuego(){
-        incognitasSalas[0] = "";
-        incognitasSalas[1] = "";
-        incognitasSalas[2] = "";
-        incognitasSalas[3] = "";
+        incognitasSalas[0] = "3";
+        incognitasSalas[1] = "3";
+        incognitasSalas[2] = "(x+1)(x-3)(x-2)";
+        incognitasSalas[3] = "-2(x+4)²(x-4)";
     }
     public static int GetEstadoJuego(){
         return estadoJuego;
